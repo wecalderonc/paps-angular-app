971554e22517cf7b1edfe10a79f126edc93e8a78
William Calderon <wecalderonc@unal.edu.co>
2019-04-29 23:13:34 -0500

    first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  welcome : string;
   title = 'paps-angular-app';
+  frameworks : {
+      framework: string,
+      owner : string,
+      release : string
+  }[];
+  public show:boolean = false
+
+
+  toggle(framework) {
+    framework.show = !framework.show;
+  }
+
+  constructor(){
+      this.welcome = "Puedes ocultar/mostrar cada fila!!!!"
+
+      this.frameworks = [{
+          framework : "ANGULAR",
+          owner: " GOOGLE",
+          release : "14 Septiembre 2016"
+      },
+      {
+          framework : "REACT",
+          owner: " FACEBOOK",
+          release : "Marzo 2013"
+      },
+      {
+          framework : "VUE",
+          owner: "COMMUNITY",
+          release : "Febrero 2014"
+      }]
+  };
 }
